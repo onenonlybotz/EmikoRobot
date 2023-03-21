89aeb5fdb1e3ab2a6367b474c48a8eaf4a97f3ee
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-03-21 09:48:49 +0530

    Update inlinefuncs.py

--- EmikoRobot/utils/inlinefuncs.py
+++ EmikoRobot/utils/inlinefuncs.py
@@ -115,21 +115,21 @@ async def alive_function(answers):
     bot_state = "Dead" if not await app.get_me() else "Alive"
     ubot_state = "Dead" if not await ubot2.get_me() else "Alive"
     buttons.add(
-        InlineKeyboardButton("Main bot", url="https://t.me/EmiexRobot"),
+        InlineKeyboardButton("Main bot", url="https://t.me/th3onlybot"),
         InlineKeyboardButton(
             "Go Inline!", switch_inline_query_current_chat=""
         ),
     )
 
     msg = f"""
-**[Emiko Robot ❤️](https://t.me/emikosupport):**
+**[Aaditya Robot ❤️](https://t.meTH3ONLYSUPPORT):**
 **MainBot:** `{bot_state}`
 **UserBot:** `{ubot_state}`
 **Python:** `{pyver.split()[0]}`
 **Pyrogram:** `{pyrover}`
 **MongoDB:** `{mongover}`
 **Platform:** `{sys.platform}`
-**Profiles:** [BOT](t.me/{BOT_USERNAME}) | [UBOT](t.me/excrybaby)
+**Profiles:** [BOT](t.me/{BOT_USERNAME}) | [UBOT](t.me/sexyaaditya)
 """
     answers.append(
         InlineQueryResultArticle(
